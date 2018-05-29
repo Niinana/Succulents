59ba58eab379b4e53bc6ff8bee02d1b0720775c7
Emina Krehic <eminakrehic10@gmail.com>
2018-05-29 12:50:09 +0200

    Gallery

--- script.js
+++ script.js
@@ -1,24 +1,11 @@
+/* ---------- NAVIGATION ---------- */
+
 const nav = document.querySelector('#navigation');
 const navItems = Array.from(document.querySelectorAll('[data-where]'));
-const title = "let the love grow.";
 
-let titleArray = title.split("");
 let topOfNav = nav.offsetTop;
 
-
-/* TITLE TYPING
-function typing(titleArray){
-    let ch = titleArray.shift();
-    if(ch != undefined){
-    document.querySelector(".title").innerHTML += ch;
-    setTimeout(()=>{typing(titleArray)}, 100);
-  }
-    return;
-}*/
-
-//setTimeout(()=>{typing(titleArray)}, 300);
-
-/* FIXED NAVIGATION STRECH */
+/* --- fixed strech --- */
 
 function fixNav() {
   if (window.scrollY >= topOfNav) {
@@ -30,33 +17,59 @@ function fixNav() {
 
 window.addEventListener('scroll', fixNav);
 
-
-/*NAVIGATING*/
+/* --- navigating --- */
 
 function navClicked(){
-
   const div = document.querySelector(`#${this.dataset.where}`);
-  console.log(div.offsetTop);
-  //window.scrollTo(0, div.offsetTop-nav.offsetHeight);
   window.scroll({
-  top: div.offsetTop-nav.offsetHeight,
-  behavior: "smooth"
-});
+    top: div.offsetTop-nav.offsetHeight,
+    behavior: "smooth"
+  });
 }
 
 navItems.forEach(item => item.addEventListener('click', navClicked));
 
 
 
-const letters = Array.from(document.querySelectorAll('.title span'));
+/* ---------- GALLERY ---------- */
 
-let delay = 0;
+const gallery = document.querySelector('#gallery');
+const galleryWrapper = document.querySelector('#gallery .wrapper');
+const moveBtns = document.querySelectorAll('#gallery .move');
 
+const translateValue = gallery.offsetWidth / 2.2;
+const lastImg = Array.from(document.querySelectorAll('#gallery .wrapper img')).pop();
 
+/* --- moving --- */
 
-//console.log(`${delay}s`);
-//letters.forEach(letter => {
- //console.log(letter.style.animationDelay);
-//letter.style.animationDelay = `${delay}`;
-//  delay += 0.2;}
-//);
+let translated = 0;
+
+function galleryMove(){
+  if(this.classList.contains('next')){
+    let galleryEnd = gallery.getBoundingClientRect().x + gallery.getBoundingClientRect().width;
+    let imageEnd =  lastImg.getBoundingClientRect().x + lastImg.getBoundingClientRect().width;
+    if(galleryEnd >= translateValue + translated){
+      translated += translateValue;
+      galleryWrapper.style.transform = `translate(-${translated}px, 0px)`;
+    }
+    else{
+      translated += imageEnd - galleryEnd;
+      galleryWrapper.style.transform = `translate(-${translated}px, 0px)`;
+    }
+  }
+  else if(this.classList.contains('previous')){
+    translated -= translateValue;
+    if(translated > 0){
+      galleryWrapper.style.transform = `translate(-${translated}px, 0px)`;
+      console.log(`translate(-${translated}px, 0px)`);
+    }
+    else{
+      translated=0;
+      galleryWrapper.style.transform = `translate(0px, 0px)`;
+    }
+  }
+}
+
+moveBtns.forEach(btn => btn.addEventListener('click', galleryMove));
+
+/* --- end of gallery moving --- */
